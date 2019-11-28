aebe34237d7b93585f82314af0b7e91801793482
Your name <Yourname@example.com>
2019-11-29 01:21:53 +0300

    Snapshot 2, program.cpp, funcs.h, funcs.cpp

--- program.cpp
+++ program.cpp
@@ -1,18 +1,22 @@
-//פאיכ program.cpp 
+////פאיכ program.cpp 
 #include "stdafx.h" 
 #include <iostream> 
+#include "funcs.h" 
 using namespace std; 
-double MySum(double a, double b) 
-{ 
-return a+b; 
-} 
-double MySub(double a, double b) 
-{ 
-return a - b; 
-} 
 int main() 
 { 
 cout << MySum(10, 3) << endl; 
 cout << MySub(10, 3) << endl; 
 return 0; 
+} 
+
+#include "stdafx.h" 
+#include "funcs.h" 
+double MySum(double a, double b) 
+{ 
+return a + b; 
+} 
+double MySub(double a, double b) 
+{ 
+return a - b; 
 } 
